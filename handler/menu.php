d0ea26a2eda76521eaa519ceceae5d7225791a16
Булат Муллагалиев <bulatmullagal@gmail.com>
2025-12-27 21:52:38 +0400

    1.0.0 fix 4 line

--- handler/menu.php
+++ handler/menu.php
@@ -1,7 +1,7 @@
 <?php
 if (count($argv) == 1) {
     echo ("\033[1;36m Welcome to tbot! \033[0m\n\n");
-    echo ("\033[35m Documntation: \n");
+    echo ("\033[35m Documntation: https://github.com/bulatik205/telegram-tbot\n");
     echo ("\033[36m Developer: bulatik205 \n\n");
     echo ("\033[1;3;37m Send command: >> \033[0m \033[1;32m php tbot make \033[1;3;37m << for start \033[0m");
 }
